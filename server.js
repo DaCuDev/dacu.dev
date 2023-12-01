ff6ed6e1492ec59f4a061540bb20ee94386408ef
DarkCu1 <rndm.dacu@gmail.com>
2023-12-01 01:00:42 -0500

    added static site

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const axios = require('axios')
 
-const app = express()
 const port = 3000
+const app = express()
+app.use(express.static('public'))
 
 app.get('/transcripts', async (req, res) => {
     try {
