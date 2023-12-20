57bebab803adc9221a90918d47e908cf29e22d69
DarkCu1 <rndm.dacu@gmail.com>
2023-12-20 03:02:18 -0500

    added db

--- server.js
+++ server.js
@@ -1,21 +1,41 @@
+const mongoose = require('mongoose')
 const express = require('express')
-const axios = require('axios')
+require('dotenv').config()
+const db = process.env.DB
 
 const port = 3000
 const app = express()
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+(async () => {
+    try { 
+        await mongoose.connect(db, {connectTimeoutMS: 5000})
+        console.log('Connected to MongoDB!')
+    } catch (err) {
+        console.log(err)
+    }
+})()
 
 app.get('/transcripts', async (req, res) => {
     try {
-        const url = req.query.url
-        if (!url) return res.status(400).json({ error: 'Missing "url" parameter' })
-        
-        const response = await axios.get(url)
-        const htmlContent = response.data
+        const id = req.query.id
+        if (!id) return res.status(400).json({error: 'Missing ID'})
+  
+        const { Schema, model } = mongoose
 
-        res.send(htmlContent)
-    } catch (error) {
-        res.status(500).json({ error: 'Internal server error' })
+        const transcriptSchema = new Schema({
+            ID: String,
+            File: String
+        })
+
+        const allTranscripts = model('transcriptSchema', transcriptSchema)
+        const data = await allTranscripts.findOne({ID: id})
+        if (!data) return res.status(404).json({error: 'Transcript not found'})
+
+        res.send(data.File)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({error: 'Internal server error'})
     }
 })
 
